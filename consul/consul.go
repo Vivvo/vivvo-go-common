71267f509d028ab25bbca677d97666b6110b2a5f
Brett Mayson <brett@vivvo.com>
2020-07-22 01:04:13 -0600

    feat: consul

---
+++ consul/consul.go
@@ -0,0 +1,25 @@
+package consul
+
+import (
+	"os"
+	"github.com/hashicorp/consul/api"
+)
+
+func getClient() (*api.Client, error) {
+	consulInsecure := os.Getenv("CONSUL_INSECURE")
+	consulHost := "consul.sd.svc.cluster.local"
+	consulHostEnv := os.Getenv("CONSUL_HOST")
+	if consulHostEnv != "" {
+		consulHost = consulHostEnv
+	}
+	consulConfig := api.Config{
+		Address:   "https://" + consulHost + ":8501",
+		TLSConfig: api.TLSConfig{InsecureSkipVerify: true},
+	}
+	if consulInsecure == "true" {
+		consulConfig = api.Config{
+			Address:   "http://" + consulHost + ":8500",
+		}
+	}
+	return api.NewClient(&consulConfig)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/hashicorp/consul/api v1.5.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pressly/goose v2.6.0+incompatible
 )
