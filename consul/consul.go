242501fb57a68a3022518a981113d62a27200d03
mp-singh <mandeep@mandeepsingh.ca>
2020-09-03 11:13:45 -0600

    feat: add address support

--- consul/consul.go
+++ consul/consul.go
@@ -1,24 +1,30 @@
 package consul
 
 import (
-	"os"
 	"github.com/hashicorp/consul/api"
+	"os"
 )
 
-func GetClient() (*api.Client, error) {
+func GetClient(address ...string) (*api.Client, error) {
 	consulInsecure := os.Getenv("CONSUL_INSECURE")
-	consulHost := "consul.sd.svc.cluster.local"
+	consulHost := "consul.server.consul"
 	consulHostEnv := os.Getenv("CONSUL_HOST")
+
 	if consulHostEnv != "" {
 		consulHost = consulHostEnv
 	}
+
+	if len(address) == 1 {
+		consulHost = address[0]
+	}
+
 	consulConfig := api.Config{
 		Address:   "https://" + consulHost + ":8501",
 		TLSConfig: api.TLSConfig{InsecureSkipVerify: true},
 	}
 	if consulInsecure == "true" {
 		consulConfig = api.Config{
-			Address:   "http://" + consulHost + ":8500",
+			Address: "http://" + consulHost + ":8500",
 		}
 	}
 	return api.NewClient(&consulConfig)
