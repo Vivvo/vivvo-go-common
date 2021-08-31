b489e5f1779ddefc0ae51f65fd93e72745a5e3eb
Brett <brett@vivvo.com>
2021-08-31 13:17:51 -0600

    maintain current behaviour by default

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -3,9 +3,9 @@ package mesh
 import "os"
 
 func Internal(path string) string {
-	if os.Getenv("USE_HTTPS") == "true" {
-		return "https://" + path
-	} else {
+	if os.Getenv("USE_HTTPS") == "false" {
 		return "http://" + path
+	} else {
+		return "https://" + path
 	}
 }
